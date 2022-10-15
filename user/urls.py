823f9b9725616e7da015d2cd18afa0b57589f524
auberr <auberr@daum.net>
2022-10-15 15:56:30 +0900

    add feature on timeattack

---
+++ user/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from user import views
+
+urlpatterns =[
+    path('signup/', views.signup),
+    path('login/', views.user_login),
+    path('home/', views.home),
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from user.models import User
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html')
+    
+    elif request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        phone = request.POST.get('phone', '')
+        address =request.POST.get('address', '')
+        user = User.objects.create_user(username=username, password=password, phone=phone, address=address)
+
+        return redirect('/login/')
+
+def user_login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/home/')
+        else:
+            return redirect('/login/')
+
+
+def home(request):
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return render(request, 'home.html')
+        else :
+            return redirect('/login/')
+    
+    elif request.method == 'POST':
+        return HttpResponse('이 요청은 POST')
+    
